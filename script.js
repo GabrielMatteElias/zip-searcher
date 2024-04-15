2636a97ccf4d06186d990d9fd36966723b4ee534
gabrielmatteelias <gabrielmatteelias@gmail.com>
2024-04-15 16:12:39 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-const photosKey = 'LRbaKDHPoZ4UDhzbmTNZWrKH5zNqQXEk1PtAvX6jxjivM09hwj7kOx6z'
 const btnSearch = document.querySelector('#submit')
+const paragrafoErroResponse = document.querySelector('#erro-response')
+
+function formatarCep() {
+
+}
 
 btnSearch.addEventListener('click', async function (e) {
     e.preventDefault();
 
-
+    paragrafoErroResponse.textContent = ''
+    
     const zipCode = document.querySelector('#zip')
-    const zipValue = Number(zipCode.value)
+    const zipValue = zipCode.value
+    console.log(typeof zipValue, zipValue);
+    const quantidadeCaracteres = zipValue.length;
+
+    if (quantidadeCaracteres < 8) {
+        paragrafoErroResponse.innerText = 'O CEP deve conter pelo menos 8 caracteres.'
+        return
+    }
 
     let zipCodeResponse = {}
 
-    await fetch(`https://viacep.com.br/ws/${zipValue}/json/`)
+    await fetch(`https://viacep.com.br/ws/${zipValue}/json`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            'content-type': 'application/json;charset=utf-8',
+        }
+    })
         .then(res => res.json())
         .then(data => {
             zipCodeResponse = data
         })
-        console.log(zipCodeResponse.localidade);
+        .catch(e => console.log(e))
+    console.log(zipCodeResponse);
 
-    // const response = await fetch(`https://api.pexels.com/v1/search?query=cidade ${zipValue}`, {
-    //     method: 'GET',
-    //     headers: {
-    //         Accept: 'application/json',
-    //         Authorization: photosKey
-    //     }
-    // })
-    // const data = await response.json()
-    // console.log(data);
+    if (zipCodeResponse && zipCodeResponse.erro === true) {
+        paragrafoErroResponse.innerText = 'O CEP informado é inválido'
+        return
+    }
+    const response = await fetch(`https://pixabay.com/api/?key=43401278-360ffe917755ed902bde80f5b&q=${zipCodeResponse.localidade}&image_type=photo`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        }
+    })
+    const data = await response.json()
+    const linkImg = data.hits[0].largeImageURL
 
     const paragrafoZipResponse = document.querySelector('.zipcode-text-response')
+
     paragrafoZipResponse.innerText = `${zipCodeResponse.logradouro} - ${zipCodeResponse.bairro}. CEP: ${zipValue}. ${zipCodeResponse.localidade} - ${zipCodeResponse.uf}`
+    document.body.style.backgroundImage = 'url(' + linkImg + ')';
 
-
-    console.log(zipCodeResponse);
 })
